53b89be3fbcb9d3079ae8627bbc9be0c282ef42c
Shinaspk <shinaspkperingati@gmail.com>
2022-11-24 08:07:21 +0530

    angularrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:LoginComponent
+  },
+
+  //dasboard
+  {
+   path:'dashboard',component:DashboardComponent
+  },
+
+  //register
+  {
+    path:'register',component:RegisterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
